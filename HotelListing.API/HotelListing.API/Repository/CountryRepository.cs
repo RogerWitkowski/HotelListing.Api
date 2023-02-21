479f6022e8b5ad195dd2089fc967010cf3a09c42
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-21 23:07:47 +0100

    implemented GetDetailsAsync method

--- HotelListing.API/HotelListing.API/Repository/CountryRepository.cs
+++ HotelListing.API/HotelListing.API/Repository/CountryRepository.cs
@@ -1,12 +1,24 @@
 ﻿using HotelListing.API.Contracts;
 using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.API.Repository
 {
     public class CountryRepository : GenericRepository<Country>, ICountryRepository
     {
+        private readonly HotelListingDbContext _dbContext;
+
         public CountryRepository(HotelListingDbContext dbContext) : base(dbContext)
         {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Country> GetDetailsAsync(int id)
+        {
+            return await _dbContext
+                .Countries
+                .Include(h => h.Hotels)
+                .FirstOrDefaultAsync(c => c.Id == id);
         }
     }
 }
