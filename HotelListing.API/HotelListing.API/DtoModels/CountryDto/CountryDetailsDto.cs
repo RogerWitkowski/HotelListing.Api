ee4bffea742c74c9f9aa0a93e73ab6750a170fb0
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-21 19:37:35 +0100

    CountryDetailsDto created

---
+++ HotelListing.API/HotelListing.API/DtoModels/CountryDto/CountryDetailsDto.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelListing.API.DtoModels.CountryDto
+{
+    public class CountryDetailsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string ShortName { get; set; }
+        public List<HotelDto.HotelDto> Hotels { get; set; }
+    }
+}
