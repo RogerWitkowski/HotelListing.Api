708c0bce9aba6ceb924a096cbcbd4b3ca16b7f7e
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-22 21:59:32 +0100

    code refactor in ApiUserDto: add inheritance from ApiLoginUserDto

--- HotelListing.API/HotelListing.API/DtoModels/UserDto/ApiUserDto.cs
+++ HotelListing.API/HotelListing.API/DtoModels/UserDto/ApiUserDto.cs
@@ -2,21 +2,12 @@
 
 namespace HotelListing.API.DtoModels.UserDto
 {
-    public class ApiUserDto
+    public class ApiUserDto : ApiLoginUserDto
     {
         [Required]
         public string FirstName { get; set; }
 
         [Required]
         public string LastName { get; set; }
-
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
-
-        [Required]
-        [DataType(DataType.Password)]
-        [StringLength(15, ErrorMessage = "Your Password is limited to {2} to {1} characters", MinimumLength = 6)]
-        public string Password { get; set; }
     }
 }
