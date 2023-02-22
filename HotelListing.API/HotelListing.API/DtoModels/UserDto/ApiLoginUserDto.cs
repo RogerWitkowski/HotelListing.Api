409e3dd26ff9cebe90053b3b4925cbfa21674133
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-22 21:58:16 +0100

    apiLoginUserDto implemented

---
+++ HotelListing.API/HotelListing.API/DtoModels/UserDto/ApiLoginUserDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.API.DtoModels.UserDto
+{
+    public class ApiLoginUserDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(15, ErrorMessage = "Your Password is limited to {2} to {1} characters", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
