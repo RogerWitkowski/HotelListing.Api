ef5ae782aa6b71f849036032b0ad2aed728e9c62
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-22 22:03:04 +0100

    register (i)authManager in services

--- HotelListing.API/HotelListing.API/Program.cs
+++ HotelListing.API/HotelListing.API/Program.cs
@@ -43,6 +43,7 @@ builder.Services.AddAutoMapper(typeof(AutoMapperConfig));
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped<ICountryRepository, CountryRepository>();
 builder.Services.AddScoped(typeof(IHotelRepository), typeof(HotelRepository));
+builder.Services.AddScoped<IAuthManager, AuthManager>();
 
 var app = builder.Build();
 
