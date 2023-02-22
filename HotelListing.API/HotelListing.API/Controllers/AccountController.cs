0f3a0871d39f644f8259fdb956f229469706a414
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-22 22:01:24 +0100

    AccountController implemented

---
+++ HotelListing.API/HotelListing.API/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using HotelListing.API.Contracts;
+using HotelListing.API.DtoModels.UserDto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelListing.API.Controllers
+{
+    [Route("api/Account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAuthManager _authManager;
+
+        public AccountController(IAuthManager authManager)
+        {
+            _authManager = authManager;
+        }
+
+        // api/Account/Register
+        [HttpPost]
+        [Route("register")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> Register([FromBody] ApiUserDto apiUserDto)
+        {
+            var errors = await _authManager.Register(apiUserDto);
+
+            if (errors.Any())
+            {
+                foreach (var error in errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> Login([FromBody] ApiLoginUserDto loginUserDto)
+        {
+            var isValidUser = await _authManager.Login(loginUserDto);
+            if (!isValidUser)
+            {
+                return Unauthorized();
+            }
+
+            return NoContent();
+        }
+    }
+}
