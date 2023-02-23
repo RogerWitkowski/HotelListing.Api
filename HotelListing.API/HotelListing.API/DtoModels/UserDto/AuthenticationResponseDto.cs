721668707e25a89d9fe08c105bb80bacd658fc25
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-23 13:48:18 +0100

    AuthenticationResponsDto implemented

---
+++ HotelListing.API/HotelListing.API/DtoModels/UserDto/AuthenticationResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelListing.API.DtoModels.UserDto
+{
+    public class AuthenticationResponseDto
+    {
+        public string UserId { get; set; }
+        public string Token { get; set; }
+    }
+}
