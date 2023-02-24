4747893b37ff052cc58be5d6a3a795fe29b5ed04
RogerWitkowski <witkowski.roger@gmail.com>
2023-02-24 21:47:51 +0100

    ExceptionMiddleware created

---
+++ HotelListing.API/HotelListing.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,64 @@
+﻿using System.Net;
+using HotelListing.API.Exceptions;
+using Newtonsoft.Json;
+
+namespace HotelListing.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(exception, $"Something went wrong while processing {context.Request.Path} - User Login Attempt for {context.Response.Body}");
+                await HandleExceptionAsync(context, exception);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
+            var errorDetails = new ErrorDetails
+            {
+                ErrorType = "Failure",
+                ErrorMessage = exception.Message,
+            };
+
+            switch (exception)
+            {
+                case NotFoundException notFoundException:
+                    statusCode = HttpStatusCode.NotFound;
+                    errorDetails.ErrorType = "Not found";
+                    break;
+
+                default:
+                    break;
+            }
+
+            string response = JsonConvert.SerializeObject(errorDetails);
+            context.Response.StatusCode = (int)statusCode;
+
+            return context.Response.WriteAsync(response);
+        }
+    }
+
+    public class ErrorDetails
+    {
+        public string ErrorType { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
