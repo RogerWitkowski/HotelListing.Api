913aeaa4c56359e12efec54e62732a0bff6df491
RogerWitkowski <witkowski.roger@gmail.com>
2023-03-01 17:47:58 +0100

    caching implemented

--- HotelListing.API/HotelListing.API/Program.cs
+++ HotelListing.API/HotelListing.API/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -94,6 +95,13 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+//!Caching
+builder.Services.AddResponseCaching(options =>
+{
+    options.MaximumBodySize = 1024;
+    options.UseCaseSensitivePaths = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -112,6 +120,21 @@ app.UseSerilogRequestLogging();
 app.UseHttpsRedirection();
 //!Cors
 app.UseCors("AllowAll");
+
+app.UseResponseCaching();
+
+app.Use(async (context, next) =>
+{
+    context.Response.GetTypedHeaders().CacheControl =
+        new CacheControlHeaderValue()
+        {
+            Public = true,
+            MaxAge = TimeSpan.FromSeconds(10)
+        };
+    context.Response.Headers[HeaderNames.Vary] = new string[] { "Accept-Encoding" };
+
+    await next();
+});
 
 app.UseAuthorization();
 
